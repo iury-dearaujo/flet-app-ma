6b1aff7b829cdc852f32e3c814e8eead570c9ed3
Iury Araujo <iury.araujo@senior.com.br>
2025-01-13 18:42:08 -0300

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+import flet
+from flet import *
+
+
+def main(page: Page):
+    page.bgcolor = "red"
+
+    page.add(
+        Column([
+            Text("Success deploy to cloudflare", size=30, weight="bold")
+
+        ])
+
+    )
+
+
+flet.app(target=main)
